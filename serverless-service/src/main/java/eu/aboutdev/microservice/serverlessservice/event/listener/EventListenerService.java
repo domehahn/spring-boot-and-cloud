ecf96c5187756590f65d1bb0dafe694b63a46446
lovetocode <develop.illuminati@gmail.com>
2023-06-29 21:54:20 +0200

    Add files via upload

--- serverless-service/src/main/java/eu/aboutdev/microservice/serverlessservice/event/listener/EventListenerService.java
+++ serverless-service/src/main/java/eu/aboutdev/microservice/serverlessservice/event/listener/EventListenerService.java
@@ -1,8 +1,13 @@
 package eu.aboutdev.microservice.serverlessservice.event.listener;
 
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +18,26 @@ import java.util.function.Function;
 @RequiredArgsConstructor
 public class EventListenerService {
 
+    private final RabbitTemplate rabbitTemplate;
+
+    private final Tracer tracer;
+
+    private final ObservationRegistry observationRegistry;
+
+    @Value("${rabbit.backend.queue}")
+    private String eventQueue;
+
+    public void send(final String eventId) {
+        rabbitTemplate.convertAndSend(eventQueue, eventId);
+    }
+
     @RabbitListener(queues = {"q.event-queue"})
-    public void onUserRegistration(final String eventId) {
-        log.info("EventId Received: {}", eventId);
+    public void onEvent(final String eventId) {
+        Observation.createNotStarted("on-message", this.observationRegistry).observe(() -> {
+            log.info("EventId Received: {}", eventId);
+            log.info("<ACCEPTANCE_TEST> <TRACE:{}> Hello from consumer", this.tracer.currentSpan().context().traceId());
+            send(eventId);
+        });
     }
 
     @Bean
